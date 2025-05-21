b17255d24cfd6ab99115f8a4d5638e4340cb86da
kqatte <kqattte@gmail.com>
2025-05-21 11:02:39 +0300

    waeroigjeoirjg

--- javascripts/javascripts.js
+++ javascripts/javascripts.js
@@ -3,21 +3,14 @@ document.addEventListener('DOMContentLoaded', () => {
 })
 
 function OpenMenu() {
-  // Получаем все элементы с классом .trigger
   const triggers = document.querySelectorAll('.menubutton')
-
-  // Получаем полноэкранный блок и кнопку закрытия
   const fullscreenBlock = document.querySelector('.menufixed')
   const closeBtn = document.querySelector('.closebutton')
-
-  // Открытие блока при клике на триггер
   triggers.forEach((trigger) => {
     trigger.addEventListener('click', () => {
       fullscreenBlock.style.display = 'flex'
     })
   })
-
-  // Закрытие блока при клике на кнопку
   closeBtn.addEventListener('click', () => {
     fullscreenBlock.style.display = 'none'
   })
