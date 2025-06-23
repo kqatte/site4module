7ea7685fe1b99e83b2c657fe1543e153d1ab7b11
kqatte <kqattte@gmail.com>
2025-06-23 15:33:59 +0300

    dpockpekr

--- javascripts/javascripts.js
+++ javascripts/javascripts.js
@@ -16,3 +16,36 @@
 //     fullscreenBlock.style.display = 'none'
 //   })
 // }
+document.addEventListener('DOMContentLoaded', function () {
+  const imageSources = [
+    'images/zin1.svg',
+    'images/zin2.svg',
+    'images/zin3.svg',
+    'images/zin4.svg'
+  ]
+
+  let currentIndex = 0
+
+  const container = document.getElementById('container')
+
+  function addImage() {
+    const img = document.createElement('img')
+    img.src = imageSources[currentIndex % imageSources.length]
+    img.classList.add('card')
+
+    // Добавляем картинку в контейнер
+    container.appendChild(img)
+
+    // Запускаем анимацию через небольшую задержку
+    setTimeout(() => {
+      img.classList.add('show')
+    }, 10)
+
+    currentIndex++
+  }
+
+  // Подписываемся на событие клика по кнопке-изображению
+  document.getElementById('add-button').addEventListener('click', () => {
+    addImage()
+  })
+})
