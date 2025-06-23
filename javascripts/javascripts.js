61b86a98a58f992c2ba120e2a75ef468f7692074
kqatte <kqattte@gmail.com>
2025-06-23 20:11:58 +0300

    гнигшиггип

--- javascripts/javascripts.js
+++ javascripts/javascripts.js
@@ -16,36 +16,42 @@
 //     fullscreenBlock.style.display = 'none'
 //   })
 // }
+
+// афиша зин по клику
 document.addEventListener('DOMContentLoaded', function () {
   const imageSources = [
-    'images/zin1.svg',
+    'images/zin5.svg',
     'images/zin2.svg',
+    'images/zin6.svg',
+    'images/zin7.svg',
     'images/zin3.svg',
-    'images/zin4.svg'
+    'images/zin8.svg',
+    'images/zin4.svg',
+    'images/zin9.svg'
   ]
-
   let currentIndex = 0
-
-  const container = document.getElementById('container')
-
+  const container = document.getElementById('containerafi1')
   function addImage() {
     const img = document.createElement('img')
     img.src = imageSources[currentIndex % imageSources.length]
     img.classList.add('card')
-
-    // Добавляем картинку в контейнер
     container.appendChild(img)
-
-    // Запускаем анимацию через небольшую задержку
     setTimeout(() => {
       img.classList.add('show')
     }, 10)
-
     currentIndex++
   }
-
-  // Подписываемся на событие клика по кнопке-изображению
-  document.getElementById('add-button').addEventListener('click', () => {
+  document.getElementById('buttonafi1').addEventListener('click', () => {
     addImage()
+  })
+})
+
+// карточка товара
+document.addEventListener('DOMContentLoaded', function () {
+  const elements = document.querySelectorAll('.toggle-color')
+  elements.forEach((element) => {
+    element.addEventListener('click', () => {
+      element.classList.toggle('color-active')
+    })
   })
 })
